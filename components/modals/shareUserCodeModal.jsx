36bc117bb15c5b100ff26d47ee3514e7ba38c9a1
Preslav Kolev <96019707+k-preslav@users.noreply.github.com>
2025-07-06 16:37:44 +0300

    Added external share and copy to share modal

--- components/modals/shareUserCodeModal.jsx
+++ components/modals/shareUserCodeModal.jsx
@@ -1,13 +1,30 @@
-import { View, Text, Image } from 'react-native'
+import { View, Text, Image, Share, Alert } from 'react-native'
 import React from 'react'
 import ThemedModal from './themedModal'
 import ThemedText from '../common/themedText'
 import QRCodeStyled from 'react-native-qrcode-styled';
 import { Colors } from '../themes/colors';
 import BigButton from '../common/bigButton';
-import { Forward } from 'lucide-react-native';
+import { Check, Copy, CopyCheck, CopyMinus, Forward } from 'lucide-react-native';
+import HorizontalView from '../views/horizontalView';
+import ActionButton from '../common/actionButton';
+import * as Clipboard from 'expo-clipboard';
 
 const ShareUserCodeModal = ({visible=true, userCode='no code', onClose}) => {
+  const [codeCopied, setCodeCopied] = React.useState(false);
+
+  const handleShareExternal = async () => {
+    try {
+      await Share.share({
+        message: userCode,
+      });
+    }
+    catch (error) {
+      Alert.alert("Something went wrong.");
+      console.error('Error sharing user code:', error);
+    }
+  }
+
   return (
     <ThemedModal height='50%' visible={visible} onClose={onClose}>
       <View style={{position: 'absolute', top: 90, right: 30}}>
@@ -33,10 +50,26 @@ const ShareUserCodeModal = ({visible=true, userCode='no code', onClose}) => {
       </View>
 
       <View style={shareCodeStayle.fixedBottom}>
-        <BigButton 
-          icon={<Forward strokeWidth={2.5} />}
-          style={{width: '93%', height: 80}}
-        >Share</BigButton>
+        <HorizontalView style={{gap: 5}}>
+          <ActionButton 
+            icon={codeCopied ? <Check strokeWidth={2.5}/> : <Copy strokeWidth={2.5} />}
+            size={80}
+            onPress={async() => {
+              await Clipboard.setStringAsync(userCode);
+              setCodeCopied(true);
+
+              setTimeout(() => {
+                setCodeCopied(false);
+              }, 1500);
+            }}
+          />
+          <BigButton 
+            icon={<Forward strokeWidth={2.5} />}
+            style={{width: '72%', height: 80}}
+            loadingOnPress={true}
+            onPress={handleShareExternal}
+          >Share</BigButton>
+        </HorizontalView>
       </View>
     </ThemedModal>
   )
@@ -60,7 +93,7 @@ const shareCodeStayle = {
     bottom: 0,
     left: 0,
     right: 0,
-    paddingBottom: 16,
+    paddingBottom: 15,
     alignItems: 'center',
     justifyContent: 'center',
   }
